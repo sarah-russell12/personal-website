a70f5d69b4588dac88d916fbdb9b534d7af73011
sarah-russell12 <sarah.russell12@ncf.edu>
2019-05-10 14:34:08 -0400

    Update mail.php
    Added sanitization and validation to email script.

--- php/mail.php
+++ php/mail.php
@@ -1,12 +1,13 @@
 <?php
 
-define("MAIL_TO", "sarah.russell12@ncf.edu");
-
 function processMailForm() {
-	list($name, $email, $company, $submittedMessage) = validateFormInformation();
+	$formInformation = validateFormInformation();
 	
-	$header = constructHeader($email);
-	$message = constructMessage($name, $company, $submittedMessage);
+	if ($formInformation != FALSE) {
+		$header = constructHeader($formInformation["email"]);
+		$message = constructMessage($formInformation["name"], $formInformation["company"], $formInformation["message"]);
+		mail("sarah.russell12@ncf.edu", $_POST["subject"], $message, $header);
+	}
 }
 
 
@@ -19,17 +20,54 @@ function validateFormInformation() {
 	$company = validateText($_POST["company"]);
 	$message = validateMessage($_POST["message"]);
 	
-	$arr = array($name, $email, $company, $message);
-	
-	return $arr;
+	if ($name != FALSE && $email != FALSE && $company != FALSE && $message != FALSE) {
+		$arr = array("name"=>$name, "email"=>$email, "company"=>$company, "message"=>$message);
+		return $arr;
+	} else {
+		return FALSE;
+	}
 }
 
 function validateText($text) {
+	$sanitizedText = filter_var(trim($text), FILTER_SANITIZE_STRING);
 	
+	$alphaRegexp = array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/"));
+	
+	if (!empty($sanitizedText) && filter_var($sanitizedText, FILTER_VALIDATE_REGEXP, $alphaRegexp)) {
+		return $sanitizedText;
+	} else {
+		return FALSE;
+	}	
 }
 
 function validateEmail($email) {
+	$sanitizedEmail = filter_var(trim($email), FILTER_SANITIZE_EMAIL);
 	
+	if (!empty($sanitizedEmail) && filter_var($sanitizedEmail, FILTER_VALIDATE_EMAIL)) {
+		return $sanitizedEmail;
+	} else {
+		return FALSE;
+	}
+}
+
+function validateMessage($message) {
+	$sanitizedMessage = filter_var(trim($message), FILTER_SANITIZE_STRING);
+	
+	if (!empty($sanitizedMessage)) {
+		return $sanitizedMessage;
+	} else {
+		return FALSE;
+	}
+}
+
+function constructHeader($email) {
+	$header = "From: ".$email."\r\n"."X-Mailer: PHP/".phpversion();
+	return $header;
+}
+
+function constructMessage($name, $company, $submittedMessage) {
+	$message = "This is a message from ".$name." representing the company ".$company.": \r\n".$submittedMessage;
+	return $message;
 }
 
 
